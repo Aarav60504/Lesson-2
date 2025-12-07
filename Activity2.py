71500515ba8259c98a5a0e9a02fb934df18ec0ab
smunni46@gmail.com <smunni46@gmail.com>
2025-12-07 10:28:19 -0600

    typecasting

---
+++ Activity2.py
@@ -0,0 +1,11 @@
+name="penguin"
+age=15
+is_student=True
+weight=38.5 
+print("after typecasting")
+age=str(age)
+print(age)
+print("data type of age is :", age)
+weight=int(weight)
+print(weight)
+print("data type of weight is :", weight)
